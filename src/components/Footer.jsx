0d111b6a7084043d0e0ed228fe4b8fe9de64f052
6rahul9 <rahulmohature0421@gmail.com>
2024-01-06 23:39:45 +0530

    large files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,90 @@
+import { IconMail, IconPhoneCall } from "@tabler/icons-react";
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <div className="footer-content">
+            <ul className="footer-content__1">
+              <li>
+                <span>CAR</span> Rent
+              </li>
+              <li>
+                We offers a big range of vehicles for all your driving needs. We
+                have the perfect car to meet your needs.
+              </li>
+              <li>
+                <a href="tel:7770084548">
+                  <IconPhoneCall /> &nbsp; (+91) 7770084548 
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="mailto: 
+                rahulmohature0421@gmail.com"
+                >
+                  <IconMail />
+                  &nbsp; rahulmohature0421@gmail.com
+                </a>
+              </li>
+
+              <li>
+                <a
+                  style={{ fontSize: "14px" }}
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://6rahul9.github.io/My-Portfolio-/"
+                >
+                  Design by Rahul Mohuture
+                </a>
+              </li>
+            </ul>
+
+            <ul className="footer-content__2">
+              <li>Company</li>
+              <li>
+                <a href="#home">New York</a>
+              </li>
+              <li>
+                <a href="#home">Careers</a>
+              </li>
+              <li>
+                <a href="#home">Mobile</a>
+              </li>
+              <li>
+                <a href="#home">Blog</a>
+              </li>
+              <li>
+                <a href="#home">How we work</a>
+              </li>
+            </ul>
+
+            <ul className="footer-content__2">
+              <li>Working Hours</li>
+              <li>Mon - Fri: 10:00AM - 7:00PM</li>
+              <li>Sat: 10:00AM - 03:00AM</li>
+              <li>Sun: Closed</li>
+            </ul>
+
+            <ul className="footer-content__2">
+              <li>Subscription</li>
+              <li>
+                <p>Subscribe your Email address for latest news & updates.</p>
+              </li>
+              <li>
+                <input type="email" placeholder="Enter Email Address"></input>
+              </li>
+              <li>
+                <button className="submit-email">Submit</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
